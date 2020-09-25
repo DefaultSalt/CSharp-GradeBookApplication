9ccc5a1d6ab7a5e0cc24cb2cc623627959fdfe4a
Toby Salt <tobysalt@gmail.com>
2020-09-25 14:06:57 +0100

    Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -3,9 +3,36 @@
     using Enums;
     public class RankedGradeBook : BaseGradeBook
     {
+
+        int studentScore = ((score / 100) * 100);
         public RankedGradeBook(string name):base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public char GetLetterGrade (double averageGrade)
+        {
+            if (studentScore >= 80)
+            {
+                return 'A';
+            }
+            else if  (studentScore < 80 && studentScore >= 60 )
+            {
+                return 'B';
+            }
+            else if (studentScore < 60 && studentScore >= 40)
+            {
+                return 'C';
+            }
+            else if (studentScore < 40 && studentScore >= 20 )
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
         }
     }
 }
