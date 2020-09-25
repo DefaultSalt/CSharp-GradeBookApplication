75a8cc4a266b09931ea38fbff1d5cd45fb6060ea
Toby Salt <tobysalt@gmail.com>
2020-09-25 18:58:49 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,30 +34,26 @@
 
         }
 
-        public void CalculateStatistics()
+        public override void CalculateStatistics()
         {
-            if (Students.Count > 5)
+            if (Students.Count() < 5)
             {
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            }
-            else
-            {
-                base.CalculateStatistics();
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
             }
 
+            base.CalculateStatistics();
         }
 
-        public override void CalculateStudentStatistics()
+        public override void CalculateStudentStatistics(string name)
         {
-            if (Students.Count > 5)
+            if (Students.Count() < 5)
             {
-                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
             }
-            else
-            {
-                base.CalculateStudentStatistics();
 
-            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
