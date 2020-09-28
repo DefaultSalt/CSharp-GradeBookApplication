c7e2371f839a5e664cc045cdd4a134dd6a9d6382
Toby Salt <tobysalt@gmail.com>
2020-09-28 16:35:59 +0100

    Update StartingUserInterface.cs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -49,9 +49,9 @@ namespace GradeBook.UserInterfaces
             BaseGradeBook gradeBook;
 
             if (type == "standard")
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, weighted);
             if (type == "ranked")
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, weighted);
             else
             {
                 Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
